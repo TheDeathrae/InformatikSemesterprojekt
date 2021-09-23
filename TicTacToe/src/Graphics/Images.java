858891235247e259e260986c89102700f5362ba3
Hakimu1 <henri.polak@web.de>
2021-09-23 21:19:19 +0200

    resize image (unfinished)

--- TicTacToe/src/Graphics/Images.java
+++ TicTacToe/src/Graphics/Images.java
@@ -13,16 +13,24 @@ public class Images {
 	private static ArrayList<String> imagesField;
 	
 	public static BufferedImage circle; 
-	public static BufferedImage cross; 
+	public static BufferedImage cross;
+	BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+	    Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_DEFAULT);
+	    BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+	    outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+	    return outputImage;
+	}
 	
 	public Images() {
 		
 		imageCircle = "src/Images/cricle.png"; 
 		imageCross = "src/Images/Cross.png";
 		
+		
 		try {
 			circle = ImageIO.read(new File(imageCircle));
 			cross = ImageIO.read(new File(imageCross));
+			circle.resizeImage
 			
 		}
 			catch (IOException ex) {
@@ -30,4 +38,5 @@ public class Images {
 			}
 		
 	}
+	
 }
