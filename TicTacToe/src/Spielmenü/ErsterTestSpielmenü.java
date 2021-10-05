a1ef94b68ae3583d689a7d44c49d75b2d06267ed
xmt-b <xmt-b@DESKTOP-9EMH4EH.fritz.box>
2021-10-05 08:17:44 +0200

    5.10 um 8:17

--- TicTacToe/src/Spielmenü/ErsterTestSpielmenü.java
+++ TicTacToe/src/Spielmenü/ErsterTestSpielmenü.java
@@ -1,31 +1,3 @@
-package Spielmenü;
+package src.Spielmenü;
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-
-public class ErsterTestSpielmenü extends JFrame {
-    
-    public ErsterTestSpielmenü() {
-
-        initUI();
-    }
-
-    private void initUI() {
-
-        add(new Board());
-
-        setSize(250, 200);
-
-        setTitle("Application");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-    }    
-    
-    public static void main(String[] args) {
-        
-        EventQueue.invokeLater(() -> {
-            ErsterTestSpielmenü ex = new ErsterTestSpielmenü();
-            ex.setVisible(true);
-        });
-    }
-}
+import java.awt
