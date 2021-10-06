656c5d4de4aac48820ce2fbc83cdeb9ae8267e39
TheDeathrae <66434910+TheDeathrae@users.noreply.github.com>
2021-10-06 22:38:13 +0200

    Add files via upload

---
+++  InformatikSemesterprojekt/src/initalisierung.java
@@ -0,0 +1,11 @@
+public class initalisierung {
+    boolean zugMoeglich;
+    static boolean gewonnen;
+    int spieler = 1;
+    int feld[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    //click feld
+    //-> zug(feld, spieler, feld)
+    //gewinnen.gewinnPruefen(feld, zugSpieler);
+    //-> gewinn animation
+    //
+}
