0df5db5ca1d90db66f91e88e0a3074f4f88ec188
henri.polak <henri.polak@r515-hpg3-09.ad.b-fk-daos.logodidact.net>
2021-11-16 08:22:52 +0100

    für Fionn

---
+++ Brettspiele/src/Mensch/FrameMensch.java
@@ -0,0 +1,46 @@
+package Mensch;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JSplitPane;
+import javax.swing.JToolBar;
+
+
+public class FrameMensch extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrameMensch frame = new FrameMensch();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrameMensch() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 744, 418);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+	}
+
+}
